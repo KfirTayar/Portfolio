82e3edd16b8ba55424d52f3209766568663d1837
KfirTayar <kfirtayar145@gmail.com>
2024-01-22 21:32:59 +0200

    adding build

---
+++ frontend/build/precache-manifest.873cf924e2737e4b087f7d25e815b340.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98dc019a3dc05adde44f1a99cacba937",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3c4de40b8d8c8ae8795",
+    "url": "./static/css/main.5b458f79.chunk.css"
+  },
+  {
+    "revision": "c5ad1907dd68a9533939",
+    "url": "./static/js/2.b0f822f0.chunk.js"
+  },
+  {
+    "revision": "81989c72ab62ce82b3109ecc32313b6b",
+    "url": "./static/js/2.b0f822f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c4de40b8d8c8ae8795",
+    "url": "./static/js/main.6ceab204.chunk.js"
+  },
+  {
+    "revision": "00ba28359098845cda8b",
+    "url": "./static/js/runtime-main.3287d09a.js"
+  },
+  {
+    "revision": "2a0abc96dde548dfa0e2552b490e41bd",
+    "url": "./static/media/about.2a0abc96.png"
+  },
+  {
+    "revision": "929702752c1ab240f3d459dc6ae2d663",
+    "url": "./static/media/cost_manager_client_side.92970275.jpg"
+  },
+  {
+    "revision": "2c156466c563a9a197aede392dbfb85d",
+    "url": "./static/media/cost_manager_server_side.2c156466.jpg"
+  },
+  {
+    "revision": "16f19757c2c8e9259daa1cc5740ed48e",
+    "url": "./static/media/friend_on_the_road.16f19757.jpg"
+  },
+  {
+    "revision": "9ca2b10ae2e3a0cff29cf71b39197ab9",
+    "url": "./static/media/gearbox_prediction.9ca2b10a.jpg"
+  },
+  {
+    "revision": "cfe73743468ef768c7de576633dece7b",
+    "url": "./static/media/parking_meter.cfe73743.jpg"
+  },
+  {
+    "revision": "528e0dbd37b4c39471f982d14c4b99a9",
+    "url": "./static/media/pong_game.528e0dbd.jpg"
+  },
+  {
+    "revision": "fbd35089710fbabcc0cf754effa8b1db",
+    "url": "./static/media/portfolio.fbd35089.jpg"
+  },
+  {
+    "revision": "c329c70ebaf466e97c9e4f9f752accdd",
+    "url": "./static/media/secure_development.c329c70e.jpg"
+  },
+  {
+    "revision": "31ad53f6d2e53836e89a4f25d25c4dc6",
+    "url": "./static/media/text_analysis.31ad53f6.jpg"
+  }
+]);
